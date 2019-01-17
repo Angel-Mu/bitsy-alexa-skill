d1df93abe25e688319675f3f9ca894d30ffe65cc
Angel Malavar <angel.malavar@gmail.com>
2019-01-16 18:57:41 -0800

    Fix on Bitsy class

--- controllers/Bitsy.js
+++ controllers/Bitsy.js
@@ -3,25 +3,16 @@ const rp = require('request-promise');
 const { BITSO_URL } = process.env;
 
 class Bitsy {
-  constructor() {
-    this.bitsoUrl = BITSO_URL;
-  }
-
-  processRequest(options) {
-    const opt = { ...options, url: this.bitsoUrl };
-    return rp(opt);
-  }
-
-  getTickerBook(book) {
+  static getTickerBook(book) {
     const options = {
-      path: '/thicker',
+      uri: `${BITSO_URL}/ticker`,
       qs: {
         book,
       },
       method: 'GET',
       json: true,
     };
-    return this.processRequest(options);
+    return rp(options);
   }
 }
 
